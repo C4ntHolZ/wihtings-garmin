dd2dd13cbf361c005ae89d5cf699895b4573e174
Felix Klauda <felix.klauda@web.de>
2024-08-09 21:18:11 +0200

    New

---
+++ src/main/java/com/withingsgarmin/withingsgarmin/WithingsGarminApplication.java
@@ -0,0 +1,13 @@
+package com.withingsgarmin.withingsgarmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithingsGarminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithingsGarminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/withingsgarmin/withingsgarmin/WithingsGarminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withingsgarmin.withingsgarmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithingsGarminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
